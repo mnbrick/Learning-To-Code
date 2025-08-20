6ba8ae8de6d04b2d2c841a1e651e8b1244246156
mnbrick <mnbrick@me.com>
2025-08-20 16:06:50 -0400

    Create Happy Birthday.py

---
+++ python/Happy Birthday.py
@@ -0,0 +1,9 @@
+def happy_birthday(name):
+  print('Happy birthday to you')
+  print('Happy birthday to you')
+  print('Happy birthday dear ' + name )
+  print('Happy birthday to you')# Write code below 💖
+
+sname = input("what's your name?")
+
+happy_birthday(sname)
