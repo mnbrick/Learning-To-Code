bd1b9228c38926386fa2f77cb14a97d79f922b0b
mnbrick <mnbrick@me.com>
2025-08-20 16:06:12 -0400

    Delete Happy Birthday.py

--- Happy Birthday.py
+++
@@ -1,9 +0,0 @@
-def happy_birthday(name):
-  print('Happy birthday to you')
-  print('Happy birthday to you')
-  print('Happy birthday dear ' + name )
-  print('Happy birthday to you')# Write code below 💖
-
-sname = input("what's your name?")
-
-happy_birthday(sname)
