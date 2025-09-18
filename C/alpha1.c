29ae4d0416608c99a4c54f036fcd7b2edcbe4985
mnbrick <mnbrick@me.com>
2025-09-18 14:28:13 -0400

    Create alpha1.c

---
+++ C/alpha1.c
@@ -0,0 +1,13 @@
+// Use an array in a For Loop to list out its elements in an automated way.
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+    char alpha[5] = {'a', 'b', 'z', 'l', 'p'};
+    for(int i = 0; i < 5; i++)
+    {
+     if alpha
+    }
+}
